656adcf3941353a5031ec9d2d777c8c2764750a5
frontlab <contact@frontlab.kr>
2023-08-09 00:17:25 +0900

    dd

--- assets/js/initeditor.js
+++ assets/js/initeditor.js
@@ -5,41 +5,37 @@
 
 /* exported initSample */
 
-if ( CKEDITOR.env.ie && CKEDITOR.env.version < 9 )
-	CKEDITOR.tools.enableHtml5Elements( document );
+if (CKEDITOR.env.ie && CKEDITOR.env.version < 9) CKEDITOR.tools.enableHtml5Elements(document);
 
 // The trick to keep the editor in the sample quite small
 // unless user specified own height.
-CKEDITOR.config.height = 150;
+CKEDITOR.config.height = 500;
 CKEDITOR.config.width = 'auto';
 
-var initEditor = ( function() {
+var initEditor = (function () {
 	var wysiwygareaAvailable = isWysiwygareaAvailable(),
-		isBBCodeBuiltIn = !!CKEDITOR.plugins.get( 'bbcode' );
+		isBBCodeBuiltIn = !!CKEDITOR.plugins.get('bbcode');
 
-	return function() {
-		var editorElement = CKEDITOR.document.getById( 'editor' );
+	return function () {
+		var editorElement = CKEDITOR.document.getById('editor');
 
 		// :(((
-		if ( isBBCodeBuiltIn ) {
-			editorElement.setHtml(
-				'Hello world!\n\n' +
-				'I\'m an instance of [url=http://ckeditor.com]CKEditor[/url].'
-			);
+		if (isBBCodeBuiltIn) {
+			editorElement.setHtml('Hello world!\n\n' + "I'm an instance of [url=http://ckeditor.com]CKEditor[/url].");
 		}
 
 		// Depending on the wysiwygare plugin availability initialize classic or inline editor.
-		if ( wysiwygareaAvailable ) {
-			CKEDITOR.replace( 'editor', {
-				filebrowserImageUploadUrl:"./lib/ck_imageupload.php?type=Images"
-				,filebrowserUploadUrl:"./lib/ck_fileupload.php?type=Files"
-				,filebrowserFlashUploadUrl:"./lib/ck_fileupload.php?type=Flash"
-				,enterMode:'2'
-				,allowedContent:true
-			} );
+		if (wysiwygareaAvailable) {
+			CKEDITOR.replace('editor', {
+				filebrowserImageUploadUrl: '/lib/ck_imageupload.php?type=Images',
+				filebrowserUploadUrl: '/lib/ck_fileupload.php?type=Files',
+				filebrowserFlashUploadUrl: '/lib/ck_fileupload.php?type=Flash',
+				enterMode: '2',
+				allowedContent: true,
+			});
 		} else {
-			editorElement.setAttribute( 'contenteditable', 'true' );
-			CKEDITOR.inline( 'editor' );
+			editorElement.setAttribute('contenteditable', 'true');
+			CKEDITOR.inline('editor');
 
 			// TODO we can consider displaying some info box that
 			// without wysiwygarea the classic editor may not work.
@@ -49,10 +45,10 @@ var initEditor = ( function() {
 	function isWysiwygareaAvailable() {
 		// If in development mode, then the wysiwygarea must be available.
 		// Split REV into two strings so builder does not replace it :D.
-		if ( CKEDITOR.revision == ( '%RE' + 'V%' ) ) {
+		if (CKEDITOR.revision == '%RE' + 'V%') {
 			return true;
 		}
 
-		return !!CKEDITOR.plugins.get( 'wysiwygarea' );
+		return !!CKEDITOR.plugins.get('wysiwygarea');
 	}
-} )();
+})();
